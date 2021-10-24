af99acef1cc7c748864b6735df074915f63cf749
Alexander Volz <Alexander@volzit.de>
2021-10-24 21:06:05 +0200

    test fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,42 +2,42 @@
 CURRFLD=$PWD
 apt-get update -y && apt-get dist-upgrade -y
 
-if [ ! -d "~/.ssh" ]; then
-    mkdir ~/.ssh
+if [ ! -d "/root/.ssh" ]; then
+    mkdir /root/.ssh
 fi
 
-if [[ $(stat -L -c "%a" ~/.ssh) != "700"]]; then
-    chmod 777 ~/.ssh
+if [[ $(stat -L -c "%a" /root/.ssh) != "700"]]; then
+    chmod 777 /root/.ssh
 fi
 
-if [ ! -f "~/.ssh/id_rsa"]
-    ssh-keygen -f ~/.ssh/id_rsa -N ''
-    cat ~/.ssh/id_rsa.pub
+if [ ! -f "/root/.ssh/id_rsa"]
+    ssh-keygen -f /root/.ssh/id_rsa -N ''
+    cat /root/.ssh/id_rsa.pub
     while read -r -p "please add the key to github and confirm (y/n)? " response && ([ "$response" != "y" ] && [ "$response" != "Y" ])
     do
         echo "you need to confirm!"
     done
 fi
 
-if [[ $(stat -L -c "%a" ~/.ssh/id_rsa) != "600"]]; then
-    chmod 600 ~/.ssh/id_rsa
+if [[ $(stat -L -c "%a" /root/.ssh/id_rsa) != "600"]]; then
+    chmod 600 /root/.ssh/id_rsa
 fi
 
-if [[ $(stat -L -c "%a" ~/.ssh/id_rsa.pub) != "644"]]; then
-    chmod 644 ~/.ssh/id_rsa.pub
+if [[ $(stat -L -c "%a" /root/.ssh/id_rsa.pub) != "644"]]; then
+    chmod 644 /root/.ssh/id_rsa.pub
 fi
 
 apt-get install git
 
-if [ ! -d "~/Proxmox-01.lan2play.local" ]; then
-    git clone git@github.com:Lan2Play/Proxmox-01.lan2play.local.git ~/Proxmox-01.lan2play.local
+if [ ! -d "/root/Proxmox-01.lan2play.local" ]; then
+    git clone git@github.com:Lan2Play/Proxmox-01.lan2play.local.git /root/Proxmox-01.lan2play.local
 fi
 
-cd ~/Proxmox-01.lan2play.local; git pull; cd $CURRFLD
+cd /root/Proxmox-01.lan2play.local; git pull; cd $CURRFLD
 
-if [ ! -f "~/Proxmox-01.lan2play.local/firstbootstrapped"]
-    touch ~/Proxmox-01.lan2play.local/firstbootstrapped
-    ~/Proxmox-01.lan2play.local/addsecondhd.sh
+if [ ! -f "/root/Proxmox-01.lan2play.local/firstbootstrapped"]
+    touch /root/Proxmox-01.lan2play.local/firstbootstrapped
+    /root/Proxmox-01.lan2play.local/addsecondhd.sh
 fi
 
-~/Proxmox-01.lan2play.local/installserver.sh 
+/root/Proxmox-01.lan2play.local/installserver.sh 
